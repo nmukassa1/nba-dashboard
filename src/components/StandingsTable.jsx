a5ae9791e112ab2881c248bb589551e50e7d9ae3
nmukassa1 <78985121+nmukassa1@users.noreply.github.com>
2024-05-18 21:58:25 +0100

    .

--- src/components/StandingsTable.jsx
+++ src/components/StandingsTable.jsx
@@ -1,7 +1,22 @@
-import { Fragment } from "react";
+import React, { Fragment, useRef, useEffect, useState } from "react";
+import gsap from 'gsap';
 
 function StandingsTable({table}) {
     const tableHeaders = [ 'TEAM', 'W', 'L', 'PCT', 'HOME', 'AWAY', 'L10' ];
+
+    const teamRef = useRef([])
+    useEffect(() => {
+        animateTableCells()
+    }, [table])
+
+    function animateTableCells(){
+        teamRef.current.forEach((el, index) => {
+            if (el) {
+              gsap.from(el, { opacity: 0, y: 20, duration: .4, ease: 'power1.out', borderColor: 'transparent', delay: index * 0.1 });
+            }
+          });
+    }
+
     return ( 
   <table>
     <thead>
@@ -14,11 +29,16 @@ function StandingsTable({table}) {
       </tr>
     </thead>
     <tbody>
-        {table.length > 0 && (
+        {table.length > 0 && ( 
             table.map((team, index) => (
                 <Fragment key={index}>
-                    {(index === 0 || index === 15) && (<tr className="conference"><th>{index == 0 ? 'Eastern' : 'Western' }</th></tr>)}
-                    <tr>
+                    {(index === 0 || index === 15) && (
+                        <tr className="conference">
+                            <th>{index == 0 ? 'Eastern' : 'Western' }</th>
+                        </tr>
+                    )}
+
+                    <tr ref={el => teamRef.current[index] = el}>
                         <th scope="row">
                             <span>{team.conference.rank}</span>
                             <span><img className="team-logo-table" src={team.team.logo} alt="" /></span>
